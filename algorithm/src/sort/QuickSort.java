3cd36a0ab0175c380ee08843140dd6e7236e284e
zhengqiangmemeda <zhengqiangmemeda@gmail.com>
2018-03-25 20:54:05 +0800

    edit quicksort

--- algorithm/src/sort/QuickSort.java
+++ algorithm/src/sort/QuickSort.java
@@ -12,40 +12,34 @@ public class QuickSort {
 	/**
 	 * @param args
 	 */
-	public static void quickSort(int[] array,int low,int high){
-		if(low<high){
-			int pivot=array[low];
-			int i=low;
-			int j=high;
-			while(i<j){
-				
-				while(i<j && array[j]>pivot){
-					j--;
-				}
-				if(i<j){
-					array[i]=array[j];
-				}
-				
-				while(i<j && array[i]<=pivot){
-					i++;
-				}
-				if(i<j){
-					array[j]=array[i];
-				}
-				//
-			}
-		array[i]=pivot;
-		quickSort(array,low,i-1);
-		quickSort(array,i+1,high);
-		}
-	}
+    public void quickSort(int[]a, int left, int right){   
+        if(left<right){
+            int low=left;
+            int high=right;
+            int pivot=a[low];
+        	while(low<high){
+        		while(low<high && a[high]>pivot){
+                    high--;
+                }
+                a[low]=a[high];
+                while(low<high && a[low]<pivot){
+                	low++;
+                }
+                a[high]=a[low];
+            }
+            a[low]=pivot;
+            quickSort(a,left,low-1);
+            quickSort(a,low+1,right); 
+        }
+    }
 	
 	public static void main(String[] args) {
+		QuickSort q=new QuickSort();
 		// TODO Auto-generated method stub
 		int [] array={3,0,2,1,5,4,7,6,9,8};
 		System.out.println("start: "+System.currentTimeMillis());
 		//mergeSort(array);
-		quickSort(array,0,array.length-1);
+		q.quickSort(array,0,array.length-1);
 		System.out.println("e n d: "+System.currentTimeMillis());
 		for(int i:array){
 			System.out.println(i);
