aa7c8d1f79e6e83c77a1e4b554d1f169b1e1d261
zhengqiangmemeda <zhengqiangmemeda@gmail.com>
2018-03-31 19:35:07 +0800

    add

--- algorithm/src/sort/BubbleSort.java
+++ algorithm/src/sort/BubbleSort.java
@@ -12,11 +12,11 @@ public class BubbleSort {
 	/**
 	 * @param args
 	 */
-	public static void bubbleSort(int[] array){
+	public <T extends Comparable<T>>void bubbleSort(T[] array){
 		for(int j=0;j<array.length-1;j++){
 			for(int i=0;i<array.length-1-j;i++){
-					if(array[i]>array[i+1]){
-						int temp=array[i];
+					if(array[i].compareTo(array[i+1])>0){
+						T temp=array[i];
 						array[i]=array[i+1];
 						array[i+1]=temp;
 					}
@@ -25,10 +25,10 @@ public class BubbleSort {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] array={3,0,2,1,5,4,7,6,9,8};
+		BubbleSort bs=new BubbleSort();
+		Integer [] array={3,0,2,1,5,4,7,6,9,8};
 		System.out.println("start: "+System.currentTimeMillis());
-		//mergeSort(array);
-		bubbleSort(array);
+		bs.bubbleSort(array);
 		System.out.println("e n d: "+System.currentTimeMillis());
 		for(int i:array){
 			System.out.println(i);
